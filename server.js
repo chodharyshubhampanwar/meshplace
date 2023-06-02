0ba8251917510536a7ace9d39e549e575f25bb4e
Shubham <shu8hampanwar@outlook.com>
2023-06-02 13:57:05 +0530

    changes to server

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const app = express();
 
 import dotenv from "dotenv";
 dotenv.config();
-import 'express-async-errors'
-import morgan from 'morgan';
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import path from 'path'
+import "express-async-errors";
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
-
-import helmet from 'helmet'
-import xss from 'xss-clean'
-import mongoSanitize from 'express-mongo-sanitize'
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
 
 import connectDB from "./db/connect.js";
 import authRouter from "./router/authRouter.js";
@@ -22,41 +21,34 @@ import auth from "./middleware/auth.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
 
-if (process.env.NODE_ENV !== 'production')
-{app.use(morgan('dev'))}
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-
-
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-
-app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.use(express.json());
-app.use(helmet())
-app.use(xss())
-app.use(mongoSanitize())
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", auth, jobsRouter);
 
-app.get('*', function (request, response) {
-  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
-})
-
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT|| 5000;
+const port = 5000;
 
-const start = async () => {
+const start = () => {
   try {
-    await connectDB(process.env.MONGO_URL);
-
-    app.listen(port, console.log(`app is listening on port ${port}`));
+    app.listen(port, console.log(`app is listening on portt ${port}`));
   } catch (error) {
     console.log(error);
   }
